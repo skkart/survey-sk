5d84533a82b8bc2cbb07dfd50e942a99b001828b
skkart <skkarthik10@gmail.com>
2020-03-11 20:17:26 +0530

    Proxy For node server

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,6 @@
 const proxy = require('http-proxy-middleware')
 
+// Add proxy for dev env to connect to server running in 5000
 module.exports = function(app) {
   app.use(proxy(['/api/', '/auth/google'], { target: 'http://localhost:5000' }));
 }
