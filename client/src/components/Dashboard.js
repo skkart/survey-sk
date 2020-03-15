ccccbbcbceb737c941960df3b6d30cd495ce8499
skkart <skkarthik10@gmail.com>
2020-03-15 15:15:33 +0530

    Dashboard changes

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import SurveryList from './surveys/SurveyList'
 
+// Page after Oauth success
 const Dashboard = () => {
   return (
     <div>
